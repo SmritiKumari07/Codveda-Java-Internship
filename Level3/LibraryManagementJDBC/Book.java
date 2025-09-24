d9faa3afaa9bc83e90b176d655088efaa0ff3f5b
SmritiKumari07 <148000111+SmritiKumari07@users.noreply.github.com>
2025-09-24 18:49:59 +0530

    Create Book.java

---
+++ Level3/LibraryManagementJDBC/Book.java
@@ -0,0 +1,23 @@
+public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private int totalCopies;
+    private int availableCopies;
+
+    public Book(int id, String title, String author, int totalCopies, int availableCopies) {
+        this.id = id; this.title = title; this.author = author;
+        this.totalCopies = totalCopies; this.availableCopies = availableCopies;
+    }
+    public int getId() { return id; }
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+    public int getTotalCopies() { return totalCopies; }
+    public int getAvailableCopies() { return availableCopies; }
+
+    @Override
+    public String toString() {
+        return String.format("ID:%d | '%s' by %s | total: %d | available: %d",
+                id, title, author, totalCopies, availableCopies);
+    }
+}
